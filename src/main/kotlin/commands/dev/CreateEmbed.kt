0cc82750eabc29d90c977921be8698dc99b5b188
X1nto <burtkni@yandex.ru>
2021-01-03 13:17:31 +0400

    fixed embed command

--- src/main/kotlin/commands/dev/CreateEmbed.kt
+++ src/main/kotlin/commands/dev/CreateEmbed.kt
@@ -5,6 +5,7 @@ import commandhandler.CommandContext
 import commands.BaseCommand
 import commands.CommandTypes.Dev
 import ext.useArguments
+import ext.useCommandProperly
 import java.awt.Color
 
 class CreateEmbed : BaseCommand(
@@ -12,7 +13,8 @@ class CreateEmbed : BaseCommand(
     commandDescription = "Create an embed from a provided json",
     commandType = Dev,
     commandArguments = listOf("<json>"),
-    commandAliases = listOf("embed")
+    commandAliases = listOf("embed"),
+    devOnly = true
 ) {
 
     override fun execute(ctx: CommandContext) {
@@ -22,30 +24,35 @@ class CreateEmbed : BaseCommand(
             val json = args.joinToString(" ")
             channel.sendMessage(
                 embedBuilder.apply {
-                    with(Klaxon().parse<JsonEmbed>(json)) {
-                        setTitle(this?.title)
-                        setDescription(this?.description)
-                        if (this?.fields != null) {
-                            this.fields.forEach {
-                                addField(
-                                    it.title,
-                                    it.content,
-                                    false
-                                )
+                    try {
+                        with(Klaxon().parse<JsonEmbed>(json)) {
+                            setTitle(this?.title)
+                            setDescription(this?.description)
+                            if (this?.fields != null) {
+                                this.fields.forEach {
+                                    addField(
+                                        it.title,
+                                        it.content,
+                                        false
+                                    )
+                                }
+                            }
+                            setImage(this?.image)
+                            setThumbnail(this?.thumbnail)
+                            setFooter(this?.footer)
+
+                            if (this?.color != null) {
+                                setColor(Color.decode(this.color))
                             }
                         }
-                        setImage(this?.image)
-                        setThumbnail(this?.thumbnail)
-                        setFooter(this?.footer)
-
-                        if (this?.color != null) {
-                            setColor(Color.decode(this.color))
-                        }
+                    } catch (e: Exception) {
+                        channel.sendMessage("Could not create an embed").queueAddReaction()
+                        return
                     }
                 }.build()
             ).queueAddReaction()
         } else {
-            channel.useArguments(1, this)
+            channel.useCommandProperly(this)
         }
     }
 
