f5bc68842b7023553a64cfcd531aee3b0c612ca3
X1nto <burtkni@yandex.ru>
2021-10-29 21:19:03 +0400

    fix and improve emotes command

--- src/main/kotlin/commands/utility/Emote.kt
+++ src/main/kotlin/commands/utility/Emote.kt
@@ -11,38 +11,35 @@ class Emote : BaseCommand(
     commandDescription = "Get a corresponding emote link"
 ) {
 
+    data class ParsedEmote(
+        val id: String,
+        val isAnimated: Boolean
+    )
 
     override suspend fun execute(
         ctx: CommandContext
     ) {
-        val emoteRegex = "<?(a)?:?(\\w{2,32}):(\\d{17,19})>?".toRegex()
+        val emoteRegex = """<?(a)?:?(\w{2,32}):(\d{17,19})>?""".toRegex()
 
         val emotes = ctx.args["emotes"]!!.string()
 
-        val parsedEmoteIds = emoteRegex.findAll(emotes).map {
-            it.groupValues[3]
+        val parsedEmotes = emoteRegex.findAll(emotes).map {
+            ParsedEmote(
+                id = it.groups[3]!!.value,
+                isAnimated = it.groups[1] != null,
+            )
         }.toList()
-        val emoteLinks = mutableListOf<String>()
 
-        if (parsedEmoteIds.isEmpty()) {
-            ctx.respondPublic {
+        if (parsedEmotes.isEmpty()) {
+            ctx.respondEphemeral {
                 content = "No emotes were provided"
             }
             return
         }
 
-        parsedEmoteIds.forEach { emoteId ->
-            val suffix = if (emoteId.startsWith("<a")) "gif" else "png"
-            val link = "https://cdn.discordapp.com/emojis/$emoteId.$suffix"
-            emoteLinks.add(link)
-        }
-
-        val filteredEmoteLinks = emoteLinks.distinct()
-        ctx.respondPublic {
-            content = filteredEmoteLinks.joinToString("\n") {
-                if (filteredEmoteLinks.size > 1) {
-                    "<$it>"
-                } else it
+        ctx.respondEphemeral {
+            content = parsedEmotes.distinct().joinToString("\n") { emote ->
+                "https://cdn.discordapp.com/emojis/${emote.id}.${if (emote.isAnimated) "gif" else "png"}"
             }
         }
     }
