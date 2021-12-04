c6a959361706f735c0950506b00f4b4603b0b7c4
X1nto <burtkni@yandex.ru>
2021-12-04 18:54:09 +0400

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.31"
+    kotlin("jvm") version "1.6.0"
     application
 }
 
@@ -11,16 +11,17 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8", version = "1.5.31"))
-    implementation(kotlin("reflect", version = "1.5.31"))
+    val kotlinVersion = "1.6.0"
+    implementation(kotlin("stdlib-jdk8", kotlinVersion))
+    implementation(kotlin("reflect", kotlinVersion))
 
-    implementation("dev.kord:kord-core:0.8.0-M7")
+    implementation("dev.kord:kord-core:0.8.0-M8")
 
     implementation("org.apache.commons:commons-math3:3.6.1")
 
     implementation("org.litote.kmongo:kmongo:4.3.0")
 
-    val logbackVersion = "1.2.6"
+    val logbackVersion = "1.2.7"
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     implementation("ch.qos.logback:logback-core:$logbackVersion")
 
@@ -28,7 +29,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
     implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
 
-    implementation("io.insert-koin:koin-core:3.1.2")
+    implementation("io.insert-koin:koin-core:3.1.3")
 
     implementation("org.reflections:reflections:0.10.2")
 }
