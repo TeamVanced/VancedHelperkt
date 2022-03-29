66e56b613eb4817906c64e696aa8127ff322620f
X1nto <burtkni@yandex.ru>
2022-03-29 13:14:58 +0400

    remove guide commands

--- src/main/kotlin/commands/vanced/BugReport.kt
+++
@@ -1,9 +0,0 @@
-package commands.vanced
-
-import core.command.base.BaseGuideCommand
-
-class BugReport : BaseGuideCommand(
-    commandName = "bugreport",
-    commandDescription = "Report Vanced bugs. For broken toasters and such, consult your local Indian tech guru ;P",
-    jsonName = "bugreport",
-)
--- src/main/kotlin/commands/vanced/Features.kt
+++
@@ -1,9 +0,0 @@
-package commands.vanced
-
-import core.command.base.BaseGuideCommand
-
-class Features : BaseGuideCommand(
-    commandName = "features",
-    commandDescription = "Get more info about vanced features",
-    jsonName = "features",
-)
